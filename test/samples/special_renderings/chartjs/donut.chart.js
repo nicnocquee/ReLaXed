01af6915e438c2f117aaf6d700699c134efb5863
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-06-06 10:17:29 +0200

    ignore donut.chart.js in test from prettier

--- test/samples/special_renderings/chartjs/donut.chart.js
+++ test/samples/special_renderings/chartjs/donut.chart.js
@@ -1,20 +1,18 @@
-module.exports = {
+{
   type: 'doughnut',
   data: {
-    datasets: [
-      {
-        data: [10, 20, 30],
-        backgroundColor: ['#db7575', '#dbc575', '#75b0db'],
-      },
-    ],
-    labels: ['Ms Word', 'Google Docs', 'ReLaXed'],
+    datasets: [{
+      data: [10, 20, 30],
+      backgroundColor:["#db7575", "#dbc575", "#75b0db"]
+    }],
+    labels: ['Ms Word', 'Google Docs', 'ReLaXed']
   },
   options: {
-    width: 350,
-    height: 350,
+    width:350,
+    height:350,
     devicePixelRatio: 0.8,
     legend: {
-      position: 'bottom',
-    },
-  },
-}
+      position: 'bottom'
+    }
+  }
+};
